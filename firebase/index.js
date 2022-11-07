f993d09185566f19a58fe9e24c445ae968417454
Carlos Gomez <carlosandres0741@gmail.com>
2022-11-07 15:54:10 -0500

    Add: Firebase config and .env

---
+++ firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: 'shopping-manager-993f5.firebaseapp.com',
+  projectId: 'shopping-manager-993f5',
+  storageBucket: 'shopping-manager-993f5.appspot.com',
+  messagingSenderId: '951192813355',
+  appId: '1:951192813355:web:2c9fe001290f2a914fcf6e'
+}
+
+export const app = initializeApp(firebaseConfig)
