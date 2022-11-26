1ddb9deb23daf6f59c0118ac9ef2e491b93f7c39
Carlos Gomez <carlosandres0741@gmail.com>
2022-11-26 01:19:20 -0500

    Added userShop and firebase products

--- screens/Shop.jsx
+++ screens/Shop.jsx
@@ -3,18 +3,26 @@ import React, { useEffect } from 'react'
 
 import { readShop } from '../firebase/firestore'
 import { auth } from '../firebase'
+import { CustomButton } from '../components'
+import colors from '../colors'
 
-const Shop = () => {
-  const [shop, setShop] = React.useState({})
+const Shop = ({ navigation }) => {
+  const [shop, setShop] = React.useState()
 
   useEffect(() => {
     readShop(auth.currentUser.uid, setShop)
   }, [])
 
+  const handleCreateShop = () => {
+    navigation.navigate('CreateShop')
+  }
+
   return (
     <View style={styles.container}>
       {
-        shop && <Text>{shop.name}</Text>
+        shop
+          ? <Text>{shop.name}</Text>
+          : <CustomButton text="Crear tienda" onPress={handleCreateShop} />
       }
     </View>
   )
@@ -24,7 +32,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: colors.darkBlue
   }
 })
 
